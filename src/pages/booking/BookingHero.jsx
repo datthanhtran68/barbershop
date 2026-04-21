0b429a7f16120a242dfe87b54bbe0732c79967a9
Dat Tran <datthanhtran67@gmail.com>
2026-04-21 23:04:03 +0700

    fix lại reponsive các trang

--- src/pages/booking/BookingHero.jsx
+++ src/pages/booking/BookingHero.jsx
@@ -5,8 +5,7 @@ function BookingHero() {
     <section className="booking_hero">
       <h1 className="booking_hero-title">Đặt lịch hẹn</h1>
       <p className="booking_hero-desc">
-        Đặt lịch tại một trong các chi nhánh của chúng tôi. Chào đón khách vãng lai, 
-        nhưng khuyến khích đặt lịch trước để đảm bảo có chỗ.
+        Đặt lịch tại một trong các chi nhánh của chúng tôi giúp quý khách chủ động được thời gian của bản thân
       </p>
     </section>
   );
